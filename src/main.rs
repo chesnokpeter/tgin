188b9118c6fb6894612de9f5058a33326f24519e
chesnokpeter <ChesnokovP1107@yandex.ru>
2025-12-01 19:33:34 +0300

    cli

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,43 @@ mod route;
 mod tgin;
 mod update;
 mod config;
-
 mod utils;
 
 use crate::tgin::Tgin;
 use crate::config::setup::{load_config, build_updates, build_route};
 
-fn main() {
-    let conf = load_config("tgin.ron");
-    println!("Loaded config with {} workers", conf.dark_threads);
+use clap::{Arg, Command};
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Command::new("tgin")
+        .about("tgin is a telegram bot routing layer")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .help("Path to the configuration file")
+        );
+
+    let matches = cli.get_matches();
+
+    let config_path: &str = matches.get_one::<String>("file")
+        .map(|s| s.as_str())
+        .unwrap();
+
+
+    let conf = load_config(config_path); 
     let inputs = build_updates(conf.updates);
     let lb = build_route(conf.route);
 
-
     let tgin = Tgin::new(
-        inputs, 
-        lb, 
-        conf.dark_threads, 
-        conf.server_port
+        inputs,
+        lb,
+        conf.dark_threads,
+        conf.server_port,
     );
 
     tgin.run();
+
+    Ok(())
 }
